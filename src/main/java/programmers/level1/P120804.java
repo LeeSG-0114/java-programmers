8f50f264b86d2b30d892a5aaeda7a3fb5423b675
LeeSG-0114 <leesin0114@naver.com>
2026-04-08 10:22:29 +0900

    feat: 프로그래머스 - 두 수의 곱 (P120804) 완료

---
+++ src/main/java/programmers/level1/P120804.java
@@ -0,0 +1,15 @@
+package programmers.level1;
+
+public class P120804 {
+    public int solution(int num1, int num2) {
+
+        int answer = 0;
+        boolean limitCheck = ((0 <= num1 && 100 >= num1) && (0 <= num2 && 100 >= num2));
+
+        if (limitCheck) {
+            answer = num1 * num2;
+        }
+
+        return answer;
+    }
+}
---
+++ src/test/java/programmers/level1/P120804Test.java
@@ -0,0 +1,15 @@
+package programmers.level1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class P120804Test {
+
+    @Test
+    void test1(){
+        P120804 sol = new P120804();
+        assertEquals(15, sol.solution(5, 3));
+    }
+
+}
