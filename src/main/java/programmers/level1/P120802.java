4fbd403acd7e3dce9fd5bbfe0ce91586eeb11775
LeeSG-0114 <leesin0114@naver.com>
2026-04-09 10:29:13 +0900

    feat: 프로그래머스 - 두 수의 합 구하기 (P120802) 완료

---
+++ src/main/java/programmers/level1/P120802.java
@@ -0,0 +1,15 @@
+package programmers.level1;
+
+public class P120802 {
+    public int solution(int num1, int num2) {
+        int answer = -1;
+
+        boolean limitCheck = ((-50000 <= num1 && 50000 >= num1) && (-50000 <= num2 && 50000 >= num2));
+
+        if (limitCheck) {
+            answer = num1 + num2;
+        }
+
+        return answer;
+    }
+}
---
+++ src/test/java/programmers/level1/P120802Test.java
@@ -0,0 +1,15 @@
+package programmers.level1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class P120802Test {
+
+    @Test
+    void test1() {
+        P120802 sol = new P120802();
+        assertEquals(4, sol.solution(3,1));
+    }
+
+}
