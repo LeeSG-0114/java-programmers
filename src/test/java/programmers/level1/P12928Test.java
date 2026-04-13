44652e9badf9d7230f2e9ee4e68a19e9f8357838
LeeSG-0114 <leesin0114@naver.com>
2026-04-13 11:05:54 +0900

    feat: 프로그래머스 - 약수의 합 (P12928) 완료

---
+++ src/test/java/programmers/level1/P12928Test.java
@@ -0,0 +1,15 @@
+package programmers.level1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class P12928Test {
+
+    @Test
+    void test1() {
+        P12928 sol = new P12928();
+        assertEquals(28, sol.solution(12));
+    }
+
+}
