ed6468d6f7e32183f29a788c04d6266a705faec9
LeeSG-0114 <leesin0114@naver.com>
2026-04-13 10:52:38 +0900

    feat: 프로그래머스 - 자릿수 더하기 (P12931) 완료

---
+++ src/main/java/programmers/level1/P12931.java
@@ -0,0 +1,18 @@
+package programmers.level1;
+
+public class P12931 {
+    public int solution(int n) {
+        int answer = 0;
+
+        while (true) {
+            answer += n % 10;
+
+            if (n < 10)
+                break;
+
+            n = n / 10;
+
+        }
+        return answer;
+    }
+}
---
+++ src/test/java/programmers/level1/P12931Test.java
@@ -0,0 +1,15 @@
+package programmers.level1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class P12931Test {
+
+    @Test
+    void test1() {
+        P12931 sol = new P12931();
+        assertEquals(15, sol.solution(159));
+    }
+
+}
