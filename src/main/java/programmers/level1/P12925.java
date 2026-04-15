0d4b212f4d77a481378541cfd4acd8752d052c9b
LeeSG-0114 <leesin0114@naver.com>
2026-04-15 10:11:58 +0900

    feat: 프로그래머스 - 문자열을 정수로 바꾸기 (P12925) 완료

---
+++ src/main/java/programmers/level1/P12925.java
@@ -0,0 +1,10 @@
+package programmers.level1;
+
+public class P12925 {
+    public int solution(String s) {
+        int answer;
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+
+}
