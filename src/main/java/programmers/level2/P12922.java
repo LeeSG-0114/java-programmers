ecfd0e8d07c6977414c31a408b31c2186f83a3bd
LeeSG-0114 <leesin0114@naver.com>
2026-05-04 11:34:26 +0900

    feat: 프로그래머스 - 수박수박수박수박수박수? (P12922) 완료

---
+++ src/main/java/programmers/level2/P12922.java
@@ -0,0 +1,17 @@
+package programmers.level2;
+
+public class P12922 {
+    public String solution(int n) {
+        StringBuilder result = new StringBuilder();
+
+        for(int i = 1; i <= n ;i++) {
+            if(i%2 == 0){
+                result.append("박");
+            } else {
+                result.append("수");
+            }
+        }
+
+        return result.toString();
+    }
+}
